6d3d8aad624ee4fa33962863e73de29fe1ec12ac
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2025-05-25 10:33:22 +0800

    apply refresh token mechanism on request

--- cmd/gastoslog-app/services/request.ts
+++ cmd/gastoslog-app/services/request.ts
@@ -1,11 +1,27 @@
 import { ofetch } from "ofetch";
 import * as SecureStore from "expo-secure-store";
 
+let refreshingToken = false;
+let requestsQueue: { resolve: any; reject: any }[] = [];
+
+const processQueue = (error: any, token: string | null = null) => {
+  requestsQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+
+  requestsQueue = [];
+};
+
 export const request = ofetch.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
+  credentials: "include",
   timeout: 3000,
   async onRequest({ request, options }) {
     console.log("[fetch request]", request, options);
@@ -24,8 +40,45 @@ export const request = ofetch.create({
   },
   async onResponse({ request, response, options }) {
     console.log("[fetch response]", request, options);
-    if (response._data?.token) {
-      options.headers.append("Authorization", `Bearer ${response._data.token}`);
+    // Handle retry mechanism when accessToken expired with refresh token (NOTE not implemented yet)
+    if (response.status === 401) {
+      if (refreshingToken) {
+        return new Promise((resolve, reject) => {
+          requestsQueue.push({ resolve, reject });
+        })
+          .then((resp) => {
+            if (resp) {
+              ofetch(request, options);
+            }
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
+      refreshingToken = true;
+      const retry = new Promise((resolve, reject) => {
+        ofetch
+          .raw<string>("/auth/Refresh-Token", {
+            baseURL: process.env.EXPO_PUBLIC_API_URL,
+            method: "POST",
+            credentials: "include",
+          })
+          .then((resp) => {
+            processQueue(null, resp._data);
+            resolve(this);
+          })
+          .catch((err) => {
+            processQueue(err, null);
+            // logout
+            SecureStore.setItemAsync("session", "");
+            reject(this);
+          })
+          .finally(() => {
+            refreshingToken = false;
+            // resolve(this)
+          });
+      });
+      await retry;
     }
   },
 });
