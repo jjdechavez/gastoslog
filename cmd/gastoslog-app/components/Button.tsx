523b175472ff9c1f955cf70393e332a4baa469e8
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2025-06-05 15:26:09 +0800

    added button component

---
+++ cmd/gastoslog-app/components/Button.tsx
@@ -0,0 +1,46 @@
+import { TouchableOpacity, type TouchableOpacityProps } from "react-native";
+import { ThemedText, ThemedTextProps } from "./ThemedText";
+import { PicoThemeVariables } from "@/styles/pico-lime";
+
+export function Button(props: TouchableOpacityProps) {
+  return (
+    <TouchableOpacity
+      style={{
+        margin: 0,
+        overflow: "visible",
+        paddingVertical: PicoThemeVariables.formElementSpacingVertical,
+        paddingHorizontal: PicoThemeVariables.formElementSpacingVertical,
+
+        borderWidth: PicoThemeVariables.borderWidth,
+        borderStyle: "solid",
+        borderColor: PicoThemeVariables.primaryInverse,
+        borderRadius: PicoThemeVariables.borderRadius,
+
+        outline: "none",
+        backgroundColor: PicoThemeVariables.primaryBackground,
+        boxShadow: "0 0 0 rgba(0, 0, 0, 0)",
+      }}
+    >
+      {props.children}
+    </TouchableOpacity>
+  );
+}
+export function ButtonText(props: ThemedTextProps) {
+  return (
+    <ThemedText
+      style={{
+        fontFamily: "inherit",
+        textTransform: "none",
+        color: PicoThemeVariables.primaryInverse,
+        fontWeight: PicoThemeVariables.fontWeight,
+        fontSize: 16,
+        // lineHeight
+        textAlign: "center",
+        textDecorationColor: "none",
+        cursor: "pointer",
+        // transition
+      }}
+      {...props}
+    />
+  );
+}
