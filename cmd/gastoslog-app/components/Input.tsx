0185e9184cdbe76e9d2a57127134797d9263687e
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2025-06-17 11:50:30 +0800

    updated input component without label

--- cmd/gastoslog-app/components/Input.tsx
+++ cmd/gastoslog-app/components/Input.tsx
@@ -6,7 +6,6 @@ import { PicoThemeVariables } from "@/styles/pico-lime";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { ThemedView } from "./ThemedView";
 import { ThemedText } from "./ThemedText";
-import { isLoading } from "expo-font";
 
 type InputErrorProps = {
   errors?: { [x: string]: unknown };
@@ -46,8 +45,6 @@ export function InputError({ errors, name }: InputErrorProps) {
 }
 
 export interface InputProps extends TextInputProps {
-  label?: string;
-  required?: boolean;
   hasError?: boolean;
   isLoading?: boolean;
 }
@@ -58,66 +55,35 @@ export const Input = React.forwardRef<TextInput, InputProps>((props, ref) => {
   const invalidBorderColor = useThemeColor({}, "formElementInvalidBorder");
   const color = useThemeColor({}, "formElementColor");
   const placeholderColor = useThemeColor({}, "formElementPlaceholderColor");
-  const labelColor = useThemeColor({}, "text");
-  const requiredColor = useThemeColor({}, "formElementInvalidBorder");
-
-  let label = null;
-  if (props?.label) {
-    let requiredContent = null;
-    if (props?.required) {
-      requiredContent = (
-        <ThemedText
-          style={{
-            color: requiredColor,
-          }}
-        >
-          *
-        </ThemedText>
-      );
-    }
-    label = (
-      <ThemedText
-        style={{
-          marginBottom: PicoThemeVariables.spacing * 0.375,
-          color: labelColor,
-        }}
-      >
-        {props.label} {requiredContent}
-      </ThemedText>
-    );
-  }
 
   return (
-    <ThemedView>
-      {label}
-      <TextInput
-        ref={ref}
-        style={[
-          {
-            marginBottom: PicoThemeVariables.spacing,
-            paddingVertical: PicoThemeVariables.formElementSpacingVertical,
-            paddingHorizontal: PicoThemeVariables.formElementSpacingHorizontal,
-            width: "100%",
-            height:
-              16 * PicoThemeVariables.lineHeight +
-              PicoThemeVariables.formElementSpacingVertical * 2 +
-              PicoThemeVariables.borderWidth * 2,
-            color,
-            borderWidth: PicoThemeVariables.borderWidth,
-            borderStyle: "solid",
-            borderColor: props?.hasError ? invalidBorderColor : borderColor,
-            borderRadius: PicoThemeVariables.borderRadius, // 4
-            backgroundColor,
-            boxShadow: "none",
-            fontWeight: PicoThemeVariables.fontWeight,
-            // transi
-          },
-          props.isLoading ? { pointerEvents: "none" } : {},
-          props.style,
-        ]}
-        placeholderTextColor={placeholderColor}
-        {...props}
-      />
-    </ThemedView>
+    <TextInput
+      ref={ref}
+      style={[
+        {
+          marginBottom: PicoThemeVariables.spacing,
+          paddingVertical: PicoThemeVariables.formElementSpacingVertical,
+          paddingHorizontal: PicoThemeVariables.formElementSpacingHorizontal,
+          width: "100%",
+          height:
+            16 * PicoThemeVariables.lineHeight +
+            PicoThemeVariables.formElementSpacingVertical * 2 +
+            PicoThemeVariables.borderWidth * 2,
+          color,
+          borderWidth: PicoThemeVariables.borderWidth,
+          borderStyle: "solid",
+          borderColor: props?.hasError ? invalidBorderColor : borderColor,
+          borderRadius: PicoThemeVariables.borderRadius, // 4
+          backgroundColor,
+          boxShadow: "none",
+          fontWeight: PicoThemeVariables.fontWeight,
+          // transi
+        },
+        props.isLoading ? { pointerEvents: "none" } : {},
+        props.style,
+      ]}
+      placeholderTextColor={placeholderColor}
+      {...props}
+    />
   );
 });
