bf2cfec86a8be40237f806f036cfca903973aa74
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2025-06-28 15:20:57 +0800

    added themed scroll view

--- cmd/gastoslog-app/components/ThemedView.tsx
+++ cmd/gastoslog-app/components/ThemedView.tsx
@@ -1,4 +1,4 @@
-import { View, type ViewProps } from 'react-native';
+import { ScrollView, View, type ViewProps } from 'react-native';
 
 import { useThemeColor } from '@/hooks/useThemeColor';
 
@@ -11,4 +11,10 @@ export function ThemedView({ style, lightColor, darkColor, ...otherProps }: Them
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+}
+
+export function TScrollView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+
+  return <ScrollView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
