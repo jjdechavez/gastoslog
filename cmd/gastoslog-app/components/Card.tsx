3233222434b4f4dda5e3c9422886265f0834373a
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2025-06-28 15:20:33 +0800

    added card

---
+++ cmd/gastoslog-app/components/Card.tsx
@@ -0,0 +1,20 @@
+import { PicoThemeVariables } from "@/styles/pico-lime";
+import { ThemedView, type ThemedViewProps } from "./ThemedView";
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+export function Card(props: ThemedViewProps) {
+  const backgroundColor = useThemeColor({}, "background");
+  return (
+    <ThemedView
+      style={{
+        marginBottom: PicoThemeVariables.blockSpacingVertical,
+        paddingVertical: PicoThemeVariables.blockSpacingVertical,
+        paddingHorizontal: PicoThemeVariables.blockSpacingHorizontal,
+        borderRadius: PicoThemeVariables.borderRadius,
+        backgroundColor,
+      }}
+    >
+      {props.children}
+    </ThemedView>
+  );
+}
