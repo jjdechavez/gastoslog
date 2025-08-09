ac193531c710524a04e8900e6eb438d27321e4b0
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2025-08-09 16:56:45 +0800

    added condition to load env

--- internal/config/config.go
+++ internal/config/config.go
@@ -19,9 +19,10 @@ func LoadENV() {
 		if err != nil {
 			log.Fatalf("Error loading .env file: %v", err)
 		}
+		assignValuesByEnvFile()
+	} else {
+		assignValuesBySecret()
 	}
-
-	assignValues()
 }
 
 // General
@@ -41,7 +42,7 @@ var (
 	DB_URL string
 )
 
-func assignValues() {
+func assignValuesByEnvFile() {
 	// General
 	APP_ENV = envs["APP_ENV"]
 	HOST = envs["HOST"]
@@ -56,5 +57,23 @@ func assignValues() {
 		os.Exit(1)
 	} else {
 		AUTH_SECRET = envs["AUTH_SECRET"]
+	}
+}
+
+func assignValuesBySecret() {
+	// General
+	APP_ENV = os.Getenv("APP_ENV")
+	HOST = os.Getenv("HOST")
+	PORT = os.Getenv("PORT")
+
+	// Database
+	DB_URL = os.Getenv("BLUEPRINT_DB_URL")
+
+	// JWT
+	if os.Getenv("AUTH_SECRET") == "" {
+		fmt.Printf("AUTH_SECRET env missing")
+		os.Exit(1)
+	} else {
+		AUTH_SECRET = os.Getenv("AUTH_SECRET")
 	}
 }
